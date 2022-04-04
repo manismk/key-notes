749dfcce41fd6fa871db4540ce53bb0c305a2f1e
manismk <manismkmech@gmail.com>
2022-04-04 23:07:31 +0530

    refactor: Made Responsive

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,22 @@
 import {
   ArchiveOutlined,
   DeleteOutline,
+  EditOutlined,
   LabelOutlined,
   NoteAltOutlined,
 } from "@mui/icons-material";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { LabelContainer } from "../";
 import "./sidebar.css";
 import { routes } from "../../constant.js";
 import { useNotes } from "../../context";
+
+const activeLink = ({ isActive }) => ({
+  backgroundColor: isActive ? "#d14600" : "var(--white)",
+  color: isActive ? "white" : "var(--black)",
+});
+
 export const Sidebar = () => {
   const [showLabelForm, setLabelForm] = useState(false);
   const { labels } = useNotes();
@@ -17,42 +24,46 @@ export const Sidebar = () => {
   return (
     <>
       <aside className="sidebar">
-        <Link to={routes.HOME_PAGE}>
+        <NavLink style={activeLink} to={routes.HOME_PAGE}>
           <div className="sidebar--item">
             <NoteAltOutlined className="icon icon--md" />
             <p className="sidebar--title">Notes</p>
           </div>
-        </Link>
+        </NavLink>
         {labels.length ? (
           <>
             {labels.map((label) => (
-              <Link to={`${routes.LABEL_PAGE}/${label}`} key={label}>
+              <NavLink
+                style={activeLink}
+                to={`${routes.LABEL_PAGE}/${label}`}
+                key={label}
+              >
                 <div className="sidebar--item">
                   <LabelOutlined className="icon icon--md" />
                   <p className="sidebar--title">{label}</p>
                 </div>
-              </Link>
+              </NavLink>
             ))}
           </>
         ) : (
           ""
         )}
         <div className="sidebar--item" onClick={() => setLabelForm(true)}>
-          <LabelOutlined className="icon icon--md" />
+          <EditOutlined className="icon icon--md" />
           <p className="sidebar--title">Add Labels</p>
         </div>
-        <Link to={routes.ARCHIVE_PAGE}>
+        <NavLink style={activeLink} to={routes.ARCHIVE_PAGE}>
           <div className="sidebar--item">
             <ArchiveOutlined className="icon icon--md" />
             <p className="sidebar--title">Archive</p>
           </div>
-        </Link>
-        <Link to={routes.TRASH_PAGE}>
+        </NavLink>
+        <NavLink style={activeLink} to={routes.TRASH_PAGE}>
           <div className="sidebar--item">
             <DeleteOutline className="icon icon--md" />
             <p className="sidebar--title">Trash</p>
           </div>
-        </Link>
+        </NavLink>
       </aside>
       {showLabelForm && (
         <LabelContainer closeForm={() => setLabelForm(false)} />
