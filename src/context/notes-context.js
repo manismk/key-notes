65b0856ad50ac0436685ae3cd69ea73e6315cf24
manismk <manismkmech@gmail.com>
2022-04-07 15:30:29 +0530

    fix: minor-fix notes contesxt

--- src/context/notes-context.js
+++ src/context/notes-context.js
@@ -18,46 +18,52 @@ const NotesProvider = ({ children }) => {
   useEffect(() => {
     setLoading(true);
     try {
-      db.collection(`users/${user.uid}/notes`)
-        .where("isArchived", "==", false)
-        .where("isTrashed", "==", false)
-        .onSnapshot((querySnapshot) => {
-          setNotes(
-            querySnapshot.docs.map((note) => ({
-              id: note.id,
-              ...note.data(),
-            }))
-          );
-          setLoading(false);
-        });
-      db.collection(`users/${user.uid}/archives`)
-        .where("isTrashed", "==", false)
-        .onSnapshot((querySnapshot) => {
-          setOtherNotes((prev) => ({
-            ...prev,
-            archivedNotes: querySnapshot.docs.map((note) => ({
-              ...note.data(),
-            })),
-          }));
-          setLoading(false);
-        });
-      db.collection(`users/${user.uid}/trashes`).onSnapshot((querySnapshot) => {
-        setOtherNotes((prev) => ({
-          ...prev,
-          trashedNotes: querySnapshot.docs.map((note) => ({
-            ...note.data(),
-          })),
-        }));
-        setLoading(false);
-      });
-      db.collection(`users/${user.uid}/labels`).onSnapshot((querySnapshot) => {
-        setLabels(
-          querySnapshot.docs.map((userId) => userId.data().label).length
-            ? querySnapshot.docs.map((userId) => userId.data().label)[0]
-            : []
+      if (user.uid) {
+        db.collection(`users/${user.uid}/notes`)
+          .where("isArchived", "==", false)
+          .where("isTrashed", "==", false)
+          .onSnapshot((querySnapshot) => {
+            setNotes(
+              querySnapshot.docs.map((note) => ({
+                id: note.id,
+                ...note.data(),
+              }))
+            );
+            setLoading(false);
+          });
+        db.collection(`users/${user.uid}/archives`)
+          .where("isTrashed", "==", false)
+          .onSnapshot((querySnapshot) => {
+            setOtherNotes((prev) => ({
+              ...prev,
+              archivedNotes: querySnapshot.docs.map((note) => ({
+                ...note.data(),
+              })),
+            }));
+            setLoading(false);
+          });
+        db.collection(`users/${user.uid}/trashes`).onSnapshot(
+          (querySnapshot) => {
+            setOtherNotes((prev) => ({
+              ...prev,
+              trashedNotes: querySnapshot.docs.map((note) => ({
+                ...note.data(),
+              })),
+            }));
+            setLoading(false);
+          }
         );
-        setLoading(false);
-      });
+        db.collection(`users/${user.uid}/labels`).onSnapshot(
+          (querySnapshot) => {
+            setLabels(
+              querySnapshot.docs.map((userId) => userId.data().label).length
+                ? querySnapshot.docs.map((userId) => userId.data().label)[0]
+                : []
+            );
+            setLoading(false);
+          }
+        );
+      }
     } catch (e) {
       console.log("Error in getting initial notes data", e);
       setLoading(false);
